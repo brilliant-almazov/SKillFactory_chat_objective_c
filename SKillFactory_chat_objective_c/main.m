2a9db61703b76c53550d3d9c28a905489c887112
Anton Brilliantov <brilliantov.anton@gmail.com>
2020-08-03 23:53:26 +0300

    NSNumber property

--- SKillFactory_chat_objective_c/main.m
+++ SKillFactory_chat_objective_c/main.m
@@ -38,6 +38,58 @@ int main(int argc, char *argv[]) {
   NSString *msg2senderUid = @"1cb4bf76-27d8-4643-af94-7d0505417e30";
   NSDate *msg2sentTime = [NSDate dateWithTimeIntervalSince1970:-4194575947];
   BOOL msg2isRead = NO;
+  NSDate *test = [NSDate now];
+  double seconds = [test timeIntervalSince1970];
+
+  NSLog(@"now: %@", test);
+  NSLog(@"seconds: %lf", seconds);
+  NSLog(@"msg2sentTime: %@", msg2sentTime);
+
+  NSString *user1Name;
+  int user1Age;
+  NSString *user1Uid;
+  NSDate *lastSeen;
+
+  user1Name = @"Пушкин";
+  user1Age = 37;
+  user1Uid = @"0cea83b4-80e8-4b3c-9b12-66c58799c207";
+  lastSeen = [NSDate dateWithTimeIntervalSince1970:-4194576667];
+
+  const int maxMessageLength = 140;
+
+  int use2age = 25;
+
+  float averageUserAge = (user1Age + use2age) / 2;
+
+  NSLog(@"(37+25)/2 = %f", averageUserAge);
+  user1Age = 36;
+  averageUserAge = (user1Age + use2age) / 2;
+  NSLog(@"(36+25)/2 = %f", averageUserAge);
+
+ /* int a = 5;
+  float b = 2;
+  int c = a / b;
+
+  NSLog(@"%i", c);*/
+
+  int startIndex = 0;
+  ++startIndex;
+  startIndex++;
+
+  NSLog(@"Без скобок: %d", 2 + 5 * 3);
+  NSLog(@"Со скобками: %d", (2 + 5) * 3);
+
+  NSInteger a = 5;
+  NSInteger b = 2;
+  NSInteger c = -(a % b) * 2;
+
+  NSLog(@"%li", c);
+
+
+  NSNumber *num1 = @37.5;
+  NSLog(@"num1: %@", num1);
+  NSLog(@"num1.intValue: %li", num1.integerValue);
+  NSLog(@"num1.floatValue: %f", num1.floatValue);
 
   NSString *appDelegateClassName;
   @autoreleasepool {
